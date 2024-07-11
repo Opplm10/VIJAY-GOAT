2dcd3db67f603e0b5bf83880068f9504a8d738b4
Opplm10 <154755466+Opplm10@users.noreply.github.com>
2024-07-11 21:41:36 +0400

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -55,7 +55,7 @@ async def start(client, message):
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker(
-            sticker=(random.choice(STICKER))
+            sticker=(random.choice(STICKER),
             caption=script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
@@ -78,7 +78,7 @@ async def start(client, message):
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker(
-            sticker=(random.choice(STICKER))
+            sticker=(random.choice(STICKER),
             caption=script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
