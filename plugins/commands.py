4811c45c3ae5681f8ec8b925a684d482dc814710
Opplm10 <154755466+Opplm10@users.noreply.github.com>
2024-07-11 22:04:38 +0400

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -59,8 +59,8 @@ async def start(client, message):
             caption==script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup==reply_markup,
             parse_mode==enums.ParseMode.HTML,
-        )
-        return
+      )
+      return
             
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help", "start", "hehe"]:
         if message.command[1] == "subscribe":
@@ -83,8 +83,8 @@ async def start(client, message):
             caption==script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup==reply_markup,
             parse_mode==enums.ParseMode.HTML,
-        )
-        return
+    )
+    return
         
     kk, file_id = message.command[1].split("_", 1) if "_" in message.command[1] else (False, False)
     pre = ('checksubp' if kk == 'filep' else 'checksub') if kk else False
